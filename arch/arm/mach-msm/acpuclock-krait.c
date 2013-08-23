5936a26f2f3a57a0eda612db67b9825649bb4e92
Hieu Phan <aznrice2k4@gmail.com>
2013-08-23 00:31:33 -0500

    Voltage Control: for HTC One X - codename evita - hybrid kernel m7 ge base
    This is a mixture of reverts of the following commits
    
    Undervolt: add checks to disallow too extreme voltages, min and max.
    5df9daa83862b545916e4514877fd3de8e549259
    
    Undervolt: few minor changes, mostly for clarity.
    f6260f9dd84718da05dfe1f88503a64a0072d810
    
    Undervolt: clean it up, make it elegant and remove the constrain of f…
    a005b7ac7e1dd4cfadd7af3f53f046eb7860f438
    and a partial revert of
    
    Mako (absolute) Voltage control.
    8fe9b3549a67f96779d5cc42dd094fc754e63eec
    
    a cherry-pick of
    
    Voltage Control: add sysfs interface
    723c1a6c1138d6e0a283d85e2346d1520974aa45
    
    and finally a partial cherry-pick of
    
    Voltage Control: initial voltage control for MSM S4-Pro SOC
    7a13af42fb1617bad231ca58ced7d6be3be36563
    
    Change-Id: I2ed2812123411012708cca69a749a3e3beb4c14a
    Signed-off-by: Hieu Phan <aznrice2k4@gmail.com>

--- arch/arm/mach-msm/acpuclock-krait.c
+++ arch/arm/mach-msm/acpuclock-krait.c
@@ -928,70 +928,53 @@ static void __init bus_init(const struct l2_level *l2_level)
 	if (ret)
 		dev_err(drv.dev, "initial bandwidth req failed (%d)\n", ret);
 }
-
+#define MIN_VDD_SC     600000 /* uV */
 #ifdef CONFIG_USERSPACE_VOLTAGE_CONTROL
 
-#define MAX_VDD 1350
-#define MIN_VDD 600
-#define MIN_VDD_SC     600000 /* uV */
 
-int get_num_freqs(void)
-{
-	int i;
-	int count = 0;
-	
-	for (i = 0; drv.acpu_freq_tbl[i].use_for_scaling; i++)
-		count++;
-		
-	return count;
-}
+#define HFPLL_MIN_VDD		 700000
+#define HFPLL_MAX_VDD		1350000
 
-ssize_t acpuclk_get_vdd_levels_str(char *buf) 
-{
+ssize_t acpuclk_get_vdd_levels_str(char *buf) {
 
 	int i, len = 0;
 
 	if (buf) {
+		mutex_lock(&driver_lock);
+
 		for (i = 0; drv.acpu_freq_tbl[i].speed.khz; i++) {
-			if (drv.acpu_freq_tbl[i].use_for_scaling) {
-				len += sprintf(buf + len, "%lumhz: %i mV\n", drv.acpu_freq_tbl[i].speed.khz/1000,
-						drv.acpu_freq_tbl[i].vdd_core/1000 );
-			}
+			/* updated to use uv required by 8x60 architecture - faux123 */
+			len += sprintf(buf + len, "%8lu: %8d\n", drv.acpu_freq_tbl[i].speed.khz,
+				drv.acpu_freq_tbl[i].vdd_core );
 		}
+
+		mutex_unlock(&driver_lock);
 	}
 	return len;
 }
 
-ssize_t acpuclk_set_vdd(char *buf) 
-{
-	unsigned int cur_volt;
-	char size_cur[get_num_freqs()];
+/* updated to use uv required by 8x60 architecture - faux123 */
+void acpuclk_set_vdd(unsigned int khz, int vdd_uv) {
+
 	int i;
-    int ret = 0;
+	unsigned int new_vdd_uv;
 
-	if (!buf)
-		return -EINVAL;
-		
-	for (i = 0; i < ARRAY_SIZE(size_cur); i++) {
-		ret = sscanf(buf, "%d", &cur_volt);
+	mutex_lock(&driver_lock);
 
-		if (ret != 1)
-			return -EINVAL;
-			
-		if (cur_volt > MAX_VDD) {
-			pr_info("Voltage Control: new volt is %d and its higher than %d so we set it to MAX_VDD(%d).\n", cur_volt, MAX_VDD, MAX_VDD);
-			cur_volt = MAX_VDD;
-		} else if (cur_volt < MIN_VDD) {
-			pr_info("Voltage Control: new volt is %d and its lower than %d so we set it to MIN_VDD(%d).\n", cur_volt, MIN_VDD, MIN_VDD);
-			cur_volt = MIN_VDD;
-		}	
-				
-		drv.acpu_freq_tbl[i].vdd_core = cur_volt*1000;
-			
-		ret = sscanf(buf, "%s", size_cur);
-		buf += (strlen(size_cur)+1);
+	for (i = 0; drv.acpu_freq_tbl[i].speed.khz; i++) {
+		if (khz == 0)
+			new_vdd_uv = min(max((unsigned int)(drv.acpu_freq_tbl[i].vdd_core + vdd_uv),
+				(unsigned int)HFPLL_MIN_VDD), (unsigned int)HFPLL_MAX_VDD);
+		else if ( drv.acpu_freq_tbl[i].speed.khz == khz)
+			new_vdd_uv = min(max((unsigned int)vdd_uv,
+				(unsigned int)HFPLL_MIN_VDD), (unsigned int)HFPLL_MAX_VDD);
+		else 
+			continue;
+
+		drv.acpu_freq_tbl[i].vdd_core = new_vdd_uv;
 	}
-	return ret;
+	pr_warn("faux123: user voltage table modified!\n");
+	mutex_unlock(&driver_lock);
 }
 #endif
 
